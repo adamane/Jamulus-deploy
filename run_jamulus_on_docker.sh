4da96e6488cc863947ac97036c113de03c73c4c9
adamane <37781579+adamane@users.noreply.github.com>
2021-03-28 15:57:11 +0200

    Install docker the official way

--- run_jamulus_on_docker.sh
+++ run_jamulus_on_docker.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
-apt install docker docker.io curl -y 
+sudo apt-get update
+sudo apt-get install \
+   apt-transport-https \
+   ca-certificates \
+   curl \
+   gnupg \
+   lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+
+echo \
+ "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+
 
 docker pull grundic/jamulus
 mkdir jam
