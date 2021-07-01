5947377f9d0854ac0662b755016d3641729c616a
adamane <37781579+adamane@users.noreply.github.com>
2021-07-01 23:30:32 +0200

    Many lost hours trying to make the docker image compile on its own
    * Update Docker-tag-overwrite

--- run_jamulus_on_docker.sh
+++ run_jamulus_on_docker.sh
@@ -20,7 +20,7 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo apt upgrade -y
 
 
-docker pull ghcr.io/adamane/jamulus_contakt:3.7
+docker pull ghcr.io/adamane/jamulus_contakt:3_7_0
 mkdir jam
 mkdir jam/recordings
 curl "https://raw.githubusercontent.com/adamane/Jamulus-deploy/main/serverfiles/welcome.txt" > $(pwd)/jam/welcome.txt
@@ -34,7 +34,7 @@ sudo docker run \
     -d --rm \
     -p 22124:22124/udp \
     -v $(pwd)/jam:/jam \
-    ghcr.io/adamane/jamulus_contakt:3.7 -n -s -p 22124 -l /jam/jamulus.log -w /jam/welcome.txt -R /jam/recordings --norecord -u 50" > run_server.sh
+    ghcr.io/adamane/jamulus_contakt:3_7_0 -n -s -p 22124 -l /jam/jamulus.log -w /jam/welcome.txt -R /jam/recordings --norecord -u 50" > run_server.sh
 sudo chmod +x $(pwd)/toggle_rec.sh
 sudo chmod +x $(pwd)/run_server.sh
 sudo chmod +x $(pwd)/install_appliences.sh
