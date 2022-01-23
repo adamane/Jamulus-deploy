948547e96425d3f3bb1b939980a044dcdd0a2027
adamane <37781579+adamane@users.noreply.github.com>
2022-01-23 13:16:42 +0100

    Update run_jamulus_on_docker.sh

--- run_jamulus_on_docker.sh
+++ run_jamulus_on_docker.sh
@@ -20,7 +20,8 @@ sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 sudo apt upgrade -y
 
 
-docker pull ghcr.io/adamane/jamulus_contakt:3_7_0
+docker pull ghcr.io/adamane/jamulus_contakt:3_7_0 # Still download this for legacy
+docker pull ghcr.io/adamane/jamulus_contakt:3_8_1
 mkdir jam
 mkdir jam/recordings
 curl "https://raw.githubusercontent.com/adamane/Jamulus-deploy/main/serverfiles/welcome.txt" > $(pwd)/jam/welcome.txt
@@ -34,7 +35,17 @@ sudo docker run \
     -d --rm \
     -p 22124:22124/udp \
     -v $(pwd)/jam:/jam \
-    ghcr.io/adamane/jamulus_contakt:3_7_0 -n -s -p 22124 -l /jam/jamulus.log -w /jam/welcome.txt -R /jam/recordings --norecord -u 50" > run_server.sh
+    ghcr.io/adamane/jamulus_contakt:3_7_0 -n -s -p 22124 -l /jam/jamulus.log -w /jam/welcome.txt -R /jam/recordings --norecord -u 50" > run_server_legacy.sh
+
+echo "
+#!/bin/bash
+sudo docker run \
+    -e TZ=Europe/Berlin \
+    --name jamulus \
+    -d --rm \
+    -p 22124:22124/udp \
+    -v $(pwd)/jam:/jam \
+    ghcr.io/adamane/jamulus_contakt:3_8_1 -n -s -p 22124 -l /jam/jamulus.log -w /jam/welcome.txt -R /jam/recordings --norecord -u 50" > run_server.sh
 sudo chmod +x $(pwd)/toggle_rec.sh
 sudo chmod +x $(pwd)/run_server.sh
 sudo chmod +x $(pwd)/install_appliences.sh
